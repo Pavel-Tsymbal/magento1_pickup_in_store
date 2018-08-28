e92df74b44c29118ab78c3e7cfe9cd17ee92cf8b
Pavel <tsymbalpavel@mail.ru>
2018-08-28 18:29:59 +0300

    change

--- app/code/community/Tsymbal/Shipment/Model/Source/Stores.php
+++ app/code/community/Tsymbal/Shipment/Model/Source/Stores.php
@@ -4,13 +4,4 @@ class Tsymbal_Shipment_Model_Source_Stores
 {
     protected $_options;
 
-    public function toOptionArray()
-    {
-        if (!$this->_options) {
-            $this->_options = Mage::getResourceModel('core/store_collection')
-                ->load()->toOptionArray();
-        }
-        return $this->_options;
-    }
-
 }
