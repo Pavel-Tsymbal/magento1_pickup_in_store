897c79b4bef496e7f6fe8c58b5e9c725f746556f
Pavel <tsymbalpavel@mail.ru>
2018-08-31 16:54:32 +0300

    result

--- app/code/community/Tsymbal/Shipment/Block/Stores.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Tsymbal_Shipment_Block_Stores extends Mage_Core_Block_Template
-{
-    public function getStores()
-{
-    return unserialize(Mage::getStoreConfig('carriers/shipment/add_store'));
-}
-}
--- app/code/community/Tsymbal/Shipment/Model/Source/Stores.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Tsymbal_Shipment_Model_Source_Stores
-{
-    protected $_options;
-
-}
